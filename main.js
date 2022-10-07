fb0386bfdbac1c1c529a6f18cb08a6565342fd75
Simone Marcantel☆ <simonemarcantel04@gmail.com>
2022-10-07 10:11:01 -0500

    functions intro

---
+++ main.js
@@ -0,0 +1,15 @@
+const fillGasTank = (gallons) => {
+    
+    
+    
+    if (gallons > 15) {
+        console.log("you cant put that mush gas in the car")
+    } else 
+    {
+        console.log(`I filled the tank with ${gallons} of gas`)
+
+        }
+}
+ 
+fillGasTank(17)
+
